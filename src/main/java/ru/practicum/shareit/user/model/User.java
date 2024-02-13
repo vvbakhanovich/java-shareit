e1c26399eb52d28c4ec40ff03bb15bacb0186e5f
vvbakhanovich <vv.bakh@yandex.ru>
2024-02-13 13:44:16 +0300

    refactor: Инициализировать items при создании

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -5,8 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.shareit.item.model.Item;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -17,5 +17,5 @@ public class User {
     private long id;
     private String name;
     private String email;
-    private List<Item> items;
+    private final List<Long> items = new ArrayList<>();
 }
