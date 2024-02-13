5d2dfd6347c66ca0f058ff87b63df78bd5dd45ef
vvbakhanovich <vv.bakh@yandex.ru>
2024-02-13 10:50:27 +0300

    feat: Добавить валидацию формата email

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Data
@@ -15,6 +16,7 @@ public class UserDto {
     private long id;
     @NotBlank(message = "Должно быть указано имя пользователя.")
     private String name;
-    @NotBlank(message = "Должен быть email.")
+    @NotBlank(message = "Должен быть указан email.")
+    @Email(message = "Некорректный формат электронной почты.", regexp = "^[\\w\\-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
     private String email;
 }
