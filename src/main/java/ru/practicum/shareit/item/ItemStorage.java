bd05040ca7497ad2b2f5c5f95fe519c4b6e02abb
vvbakhanovich <vv.bakh@yandex.ru>
2024-02-13 13:47:01 +0300

    feat: Реализовать ItemStorage

---
+++ src/main/java/ru/practicum/shareit/item/ItemStorage.java
@@ -0,0 +1,18 @@
+package ru.practicum.shareit.item;
+
+import ru.practicum.shareit.item.dto.ItemUpdateDto;
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.List;
+
+public interface ItemStorage {
+    Item save(Item itemDto);
+
+    Item findById(long itemId);
+
+    Item update(long itemId, ItemUpdateDto itemUpdateDto);
+
+    List<Item> findAllByUserId(long userId);
+
+    List<Item> search(String text);
+}
---
+++ src/main/java/ru/practicum/shareit/item/ItemStorageImpl.java
@@ -0,0 +1,74 @@
+package ru.practicum.shareit.item;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import ru.practicum.shareit.item.dto.ItemUpdateDto;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.shared.IdGenerator;
+import ru.practicum.shareit.shared.exception.NotFoundException;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemStorageImpl implements ItemStorage {
+
+    private final IdGenerator<Long> idGenerator;
+    private final Map<Long, Item> items = new HashMap<>();
+
+    @Override
+    public Item save(final Item item) {
+        final Long id = idGenerator.generateId();
+        item.setId(id);
+        items.put(id, item);
+        return item;
+    }
+
+    @Override
+    public Item findById(long itemId) {
+        if (!items.containsKey(itemId)) {
+            throw new NotFoundException("Вещь с id '" + itemId + "' не найдена.");
+        }
+        return items.get(itemId);
+    }
+
+    @Override
+    public Item update(long itemId, ItemUpdateDto itemUpdateDto) {
+        if (!items.containsKey(itemId)) {
+            throw new NotFoundException("Вещь с id '" + itemId + "' не найдена.");
+        }
+        Item storedItem = items.get(itemId);
+        if (itemUpdateDto.getName() != null) {
+            storedItem.setName(itemUpdateDto.getName());
+        }
+        if (itemUpdateDto.getDescription() != null) {
+            storedItem.setDescription(itemUpdateDto.getDescription());
+        }
+        if (itemUpdateDto.getAvailable() != null) {
+            storedItem.setAvailable(itemUpdateDto.getAvailable());
+        }
+        return storedItem;
+    }
+
+    @Override
+    public List<Item> findAllByUserId(long userId) {
+        return items.values().stream()
+                .filter(item -> item.getOwner() == userId)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Item> search(String text) {
+        if (text.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return items.values().stream()
+                .filter((item -> item.getName().toLowerCase().contains(text) ||
+                        item.getDescription().toLowerCase().contains(text.toLowerCase()) && item.isAvailable()))
+                .collect(Collectors.toList());
+    }
+}
