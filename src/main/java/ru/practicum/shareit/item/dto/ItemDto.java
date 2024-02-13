14042e85b316186d0f167a2eae1b84fc5419e60b
vvbakhanovich <vv.bakh@yandex.ru>
2024-02-13 09:13:41 +0300

    feat: Создать Item и ItemDto

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,23 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ItemDto {
+    private long id;
+    @NotBlank(message = "У вещи должно быть название.")
+    private String name;
+    @NotBlank(message = "У вещи должно быть описание.")
+    private String description;
+    @NotNull(message = "У вещи должен быть указан статус доступности.")
+    private boolean available;
 }
