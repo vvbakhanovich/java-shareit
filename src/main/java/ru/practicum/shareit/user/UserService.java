20f905230a32da72005fdc6c2099f02137f2f27a
vvbakhanovich <vv.bakh@yandex.ru>
2024-02-13 10:49:54 +0300

    feat: Реализовать UserService

---
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -0,0 +1,20 @@
+package ru.practicum.shareit.user;
+
+import org.springframework.stereotype.Service;
+import ru.practicum.shareit.user.dto.UserUpdateDto;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.util.List;
+
+@Service
+public interface UserService {
+    UserDto addUser(UserDto userDto);
+
+    UserDto updateUser(long userId, UserUpdateDto userUpdateDto);
+
+    UserDto findUserById(long userId);
+
+    List<UserDto> findAllUsers();
+
+    void deleteUserById(long userId);
+}
