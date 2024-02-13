4c68e1d113eafcc3271335e792a8210f062a0d45
vvbakhanovich <vv.bakh@yandex.ru>
2024-02-13 10:50:06 +0300

    feat: Реализовать UserStorage

---
+++ src/main/java/ru/practicum/shareit/user/UserStorage.java
@@ -0,0 +1,18 @@
+package ru.practicum.shareit.user;
+
+import ru.practicum.shareit.user.dto.UserUpdateDto;
+import ru.practicum.shareit.user.model.User;
+
+import java.util.List;
+
+public interface UserStorage {
+    User save(User user);
+
+    User update(long userId, UserUpdateDto updateField);
+
+    User findById(long userId);
+
+    List<User> findAll();
+
+    void deleteById(long userId);
+}
