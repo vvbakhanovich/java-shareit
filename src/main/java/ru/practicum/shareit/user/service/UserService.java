63096cbbecd619eeaa0f3e1a6ff573db5ee1234d
vvbakhanovich <vv.bakh@yandex.ru>
2024-02-15 09:00:49 +0300

    fix: Удалить аннотацию Service

--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -1,12 +1,10 @@
 package ru.practicum.shareit.user.service;
 
-import org.springframework.stereotype.Service;
-import ru.practicum.shareit.user.dto.UserUpdateDto;
 import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.dto.UserUpdateDto;
 
 import java.util.List;
 
-@Service
 public interface UserService {
     UserDto addUser(UserDto userDto);
 
