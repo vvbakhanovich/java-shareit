c6d850a71ec810fa60d6e56b95bca05ec18317fc
vvbakhanovich <vv.bakh@yandex.ru>
2024-02-28 17:08:53 +0300

    refactor: Использовать методы JpaRepository

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.item.service;
 
+import com.google.common.collect.Lists;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -7,11 +9,14 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ItemUpdateDto;
 import ru.practicum.shareit.item.mapper.ItemMapper;
 import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.model.QItem;
 import ru.practicum.shareit.item.storage.ItemStorage;
 import ru.practicum.shareit.shared.exception.NotFoundException;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.storage.UserStorage;
 
+import javax.transaction.Transactional;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -23,30 +28,44 @@ public class ItemServiceImpl implements ItemService {
     private final ItemMapper itemMapper;
 
     @Override
+    @Transactional
     public ItemDto addItem(long userId, ItemDto itemDto) {
-        User owner = userStorage.findById(userId);
+        User owner = userStorage.findById(userId)
+                .orElseThrow(() -> new NotFoundException("Пользователь с id '" + userId + "' не найден."));
         Item item = itemMapper.toModel(itemDto);
         item.setOwner(owner);
-        userStorage.addItemToUser(userId, item);
-        Item addedItem = itemStorage.save(userId, item);
+        Item addedItem = itemStorage.save(item);
         log.info("Пользователь с id '{}' добавил новую вещь c id '{}'.", userId, addedItem.getId());
         return itemMapper.toDto(addedItem);
     }
 
     @Override
+    @Transactional
     public ItemDto updateItem(long userId, long itemId, ItemUpdateDto itemUpdateDto) {
-        userStorage.findById(userId);
-        Item item = itemStorage.findById(itemId);
+        userStorage.findById(userId)
+                .orElseThrow(() -> new NotFoundException("Пользователь с id '" + userId + "' не найден."));
+        Item item = itemStorage.findById(itemId)
+                .orElseThrow(() -> new NotFoundException("Вещь с id '" + itemId + "' не найдена."));
         if (item.getOwner().getId() != userId) {
             throw new NotFoundException("У пользователя с id '" + userId + "' не найдена вещь с id '" + itemId + "'.");
         }
-        Item updatedItem = itemStorage.update(itemId, itemUpdateDto);
+        if (itemUpdateDto.getName() != null) {
+            item.setName(itemUpdateDto.getName());
+        }
+        if (itemUpdateDto.getDescription() != null) {
+            item.setDescription(itemUpdateDto.getDescription());
+        }
+        if (itemUpdateDto.getAvailable() != null) {
+            item.setAvailable(itemUpdateDto.getAvailable());
+        }
+        Item updatedItem = itemStorage.save(item);
         return itemMapper.toDto(updatedItem);
     }
 
     @Override
     public ItemDto findItemById(long itemId) {
-        Item item = itemStorage.findById(itemId);
+        Item item = itemStorage.findById(itemId)
+                .orElseThrow(() -> new NotFoundException("Вещь с id '" + itemId + "' не найдена."));
         log.info("Получение вещи с id '{}'.", itemId);
         return itemMapper.toDto(item);
     }
@@ -54,15 +73,21 @@ public class ItemServiceImpl implements ItemService {
     @Override
     public List<ItemDto> findAllItemsByUserId(long userId) {
         userStorage.findById(userId);
-        List<Item> items = itemStorage.findAllByUserId(userId);
+        List<Item> items = itemStorage.findAllByOwnerId(userId);
         log.info("Получение всех вещей пользователя с id '{}'.", userId);
         return itemMapper.toDtoList(items);
     }
 
     @Override
     public List<ItemDto> searchItems(String text) {
-        List<Item> searchResult = itemStorage.search(text);
         log.info("Поиск вещей по запросу: {}.", text);
-        return itemMapper.toDtoList(searchResult);
+        if (text.isBlank()) {
+            return Collections.emptyList();
+        }
+        BooleanExpression nameContains = QItem.item.name.containsIgnoreCase(text);
+        BooleanExpression descriptionContains = QItem.item.description.containsIgnoreCase(text);
+        BooleanExpression isAvailable = QItem.item.available.eq(true);
+        Iterable<Item> searchResult = itemStorage.findAll(nameContains.or(descriptionContains).and(isAvailable));
+        return itemMapper.toDtoList(Lists.newArrayList(searchResult));
     }
 }
--- src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -3,12 +3,14 @@ package ru.practicum.shareit.user.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.practicum.shareit.shared.exception.NotFoundException;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.dto.UserUpdateDto;
 import ru.practicum.shareit.user.mapper.UserMapper;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.storage.UserStorage;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -20,6 +22,7 @@ public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
 
     @Override
+    @Transactional
     public UserDto addUser(final UserDto userDto) {
         final User user = userMapper.toModel(userDto);
         final User addedUser = userStorage.save(user);
@@ -28,15 +31,25 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public UserDto updateUser(final long userId, final UserUpdateDto userUpdateDto) {
-        final User updatedUserFromDb = userStorage.update(userId, userUpdateDto);
+        User storedUser = userStorage.findById(userId)
+                .orElseThrow(() -> new NotFoundException("Пользователь с id '" + userId + "' не найден."));
+        if (userUpdateDto.getName() != null) {
+            storedUser.setName(userUpdateDto.getName());
+        }
+        if (userUpdateDto.getEmail() != null) {
+            storedUser.setEmail(userUpdateDto.getEmail());
+        }
         log.info("Обновление пользователя с id '{}'.", userId);
-        return userMapper.toDto(updatedUserFromDb);
+        userStorage.save(storedUser);
+        return userMapper.toDto(storedUser);
     }
 
     @Override
     public UserDto findUserById(final long userId) {
-        final User user = userStorage.findById(userId);
+        final User user = userStorage.findById(userId)
+                .orElseThrow(() -> new NotFoundException("Пользователь с id '" + userId + "' не найден."));
         log.info("Получение пользователя с id '{}.", userId);
         return userMapper.toDto(user);
     }
