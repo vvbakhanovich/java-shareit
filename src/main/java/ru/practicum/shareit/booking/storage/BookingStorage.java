317562bacdc71e56627bc884195b49b17e5ca739
vvbakhanovich <vv.bakh@yandex.ru>
2024-03-01 15:54:05 +0300

    refactor: Добавить join fetch

--- src/main/java/ru/practicum/shareit/booking/storage/BookingStorage.java
+++ src/main/java/ru/practicum/shareit/booking/storage/BookingStorage.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.booking.storage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import ru.practicum.shareit.booking.model.Booking;
 
@@ -8,8 +9,11 @@ import java.util.Collection;
 import java.util.List;
 
 public interface BookingStorage extends JpaRepository<Booking, Long>, QuerydslPredicateExecutor<Booking> {
+
+    @Query("SELECT b FROM Booking b JOIN FETCH b.item i JOIN FETCH b.booker u WHERE i.id = ?1")
     List<Booking> findAllByItemId(Long itemId);
 
+    @Query("select b from Booking b JOIN FETCH b.item i JOIN FETCH b.booker u WHERE i.id = ?1 AND u.id = ?2")
     List<Booking> findAllByItemIdAndBookerId(Long itemId, Long bookerId);
 
     List<Booking> findAllByItemIdIn(Collection<Long> itemIds);
--- src/main/java/ru/practicum/shareit/item/storage/CommentStorage.java
+++ src/main/java/ru/practicum/shareit/item/storage/CommentStorage.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item.storage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.item.model.Comment;
 
 import java.util.Collection;
@@ -9,5 +10,6 @@ import java.util.List;
 public interface CommentStorage extends JpaRepository<Comment, Long> {
     List<Comment> findAllByItemId(Long itemId);
 
+    @Query("SELECT c FROM Comment c JOIN FETCH c.item i JOIN FETCH c.author u WHERE i.id IN ?1")
     List<Comment> findAllByItemIdIn(Collection<Long> itemIds);
 }
