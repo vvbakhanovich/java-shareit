746bdd688d083dfe69b6d99b77e8253762f20985
vvbakhanovich <vv.bakh@yandex.ru>
2024-03-02 12:19:15 +0300

    refactor: Удалить JOIN FETCH

--- src/main/java/ru/practicum/shareit/booking/storage/BookingStorage.java
+++ src/main/java/ru/practicum/shareit/booking/storage/BookingStorage.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.booking.storage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import ru.practicum.shareit.booking.model.Booking;
 
@@ -10,10 +9,8 @@ import java.util.List;
 
 public interface BookingStorage extends JpaRepository<Booking, Long>, QuerydslPredicateExecutor<Booking> {
 
-    @Query("SELECT b FROM Booking b JOIN FETCH b.item i JOIN FETCH b.booker u WHERE i.id = ?1")
     List<Booking> findAllByItemId(Long itemId);
 
-    @Query("select b from Booking b JOIN FETCH b.item i JOIN FETCH b.booker u WHERE i.id = ?1 AND u.id = ?2")
     List<Booking> findAllByItemIdAndBookerId(Long itemId, Long bookerId);
 
     List<Booking> findAllByItemIdIn(Collection<Long> itemIds);
