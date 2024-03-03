8b05c3b0e2d1d92bc8d33fd05ca5c90ff955e763
vvbakhanovich <vv.bakh@yandex.ru>
2024-03-03 19:45:00 +0300

    refactor: Не использовать QuerydslPredicateExecutor

--- src/main/java/ru/practicum/shareit/item/storage/ItemStorage.java
+++ src/main/java/ru/practicum/shareit/item/storage/ItemStorage.java
@@ -2,12 +2,11 @@ package ru.practicum.shareit.item.storage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
 
-public interface ItemStorage extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
+public interface ItemStorage extends JpaRepository<Item, Long> {
     List<Item> findAllByOwnerIdOrderById(Long userId);
 
     @Query("SELECT i FROM Item i WHERE LOWER(i.name) LIKE ?1 OR LOWER(i.description) LIKE ?1 AND i.available = true")
