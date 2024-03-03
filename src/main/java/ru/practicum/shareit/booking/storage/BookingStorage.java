7f053ee4d29e3ef9ab2fe14ee26c113ae76cc360
vvbakhanovich <vv.bakh@yandex.ru>
2024-03-03 19:46:14 +0300

    feat: Реализовать findBookingById

--- src/main/java/ru/practicum/shareit/booking/storage/BookingStorage.java
+++ src/main/java/ru/practicum/shareit/booking/storage/BookingStorage.java
@@ -2,15 +2,18 @@ package ru.practicum.shareit.booking.storage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.BookingStatus;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
-public interface BookingStorage extends JpaRepository<Booking, Long>, QuerydslPredicateExecutor<Booking> {
+public interface BookingStorage extends JpaRepository<Booking, Long> {
+
+    @Query("SELECT b FROM Booking b JOIN FETCH b.item i JOIN FETCH b.booker u WHERE b.id = ?1")
+    Optional<Booking> findBookingById(Long bookingId);
 
     @Query("SELECT b FROM Booking b JOIN b.item i JOIN FETCH b.booker u WHERE i.id = ?1")
     List<Booking> findAllByItemId(Long itemId);
