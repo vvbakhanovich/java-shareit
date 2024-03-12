e4427a872be765b0df04a91b17763f26d6f5cf2a
vvbakhanovich <vv.bakh@yandex.ru>
2024-03-12 17:20:35 +0300

    feat: Реализовать ItemRequestStorage

---
+++ src/main/java/ru/practicum/shareit/request/storage/ItemRequestStorage.java
@@ -0,0 +1,19 @@
+package ru.practicum.shareit.request.storage;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import ru.practicum.shareit.request.model.ItemRequest;
+
+import java.util.List;
+
+public interface ItemRequestStorage extends JpaRepository<ItemRequest, Long> {
+
+    List<ItemRequest> findAllByRequesterIdOrderByCreatedDesc(Long requesterId);
+
+    Page<ItemRequest> findAllByRequesterIdNotOrderByCreatedDesc(long requesterId, Pageable pageable);
+
+    @Query("SELECT ir FROM ItemRequest ir LEFT JOIN FETCH ir.items i JOIN ir.requester r WHERE r.id = ?1 ORDER BY ir.created DESC ")
+    List<ItemRequest> findRequestsFromUser(Long requesterId);
+}
