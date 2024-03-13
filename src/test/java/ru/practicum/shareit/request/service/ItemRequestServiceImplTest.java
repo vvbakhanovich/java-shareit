3627696cd9b58c96bd9993083499c5d4207b9ad2
vvbakhanovich <vv.bakh@yandex.ru>
2024-03-13 14:05:24 +0300

    fix

--- src/test/java/ru/practicum/shareit/request/service/ItemRequestServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/request/service/ItemRequestServiceImplTest.java
@@ -1,9 +1,7 @@
 package ru.practicum.shareit.request.service;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
