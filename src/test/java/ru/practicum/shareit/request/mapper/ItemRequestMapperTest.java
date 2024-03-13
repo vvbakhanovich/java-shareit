ae08e1ed328c31fc138974d73849d7e8b2fae1a9
vvbakhanovich <vv.bakh@yandex.ru>
2024-03-13 11:02:14 +0300

    feat: Использовать ContextConfiguration

--- src/test/java/ru/practicum/shareit/request/mapper/ItemRequestMapperTest.java
+++ src/test/java/ru/practicum/shareit/request/mapper/ItemRequestMapperTest.java
@@ -1,8 +1,12 @@
 package ru.practicum.shareit.request.mapper;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ru.practicum.shareit.item.mapper.ItemMapperImpl;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.dto.AddItemRequestDto;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
@@ -13,15 +17,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {
+        ItemMapperImpl.class,
+        ItemRequestMapperImpl.class})
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ItemRequestMapperTest {
 
+    @Autowired
     private ItemRequestMapper itemRequestMapper;
-
-    @BeforeAll
-    public void init() {
-        itemRequestMapper = new ItemRequestMapperImpl();
-    }
 
     @Test
     public void mapToModelWithCreatedNotNull() {
@@ -37,12 +41,12 @@ class ItemRequestMapperTest {
     public void mapToItemRequestDtoWithItem() {
         User requester = User.builder().id(1L).build();
         Item item = Item.builder().id(2L).build();
-        ItemRequest itemRequest = ItemRequest.builder().requester(requester).build();
+        ItemRequest itemRequest = ItemRequest.builder().id(1L).requester(requester).build();
         itemRequest.addItem(item);
 
         ItemRequestDto itemRequestDto = itemRequestMapper.toDto(itemRequest);
 
-        assertThat(itemRequestDto.getItems().get(0).getRequestId(), is(1));
+        assertThat(itemRequestDto.getItems().get(0).getRequestId(), is(1L));
     }
 
 }
