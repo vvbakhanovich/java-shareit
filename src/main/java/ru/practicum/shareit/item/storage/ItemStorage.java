1428b9b2447b1d1e8efa015a91b44c8eb2e0bb6b
vvbakhanovich <vv.bakh@yandex.ru>
2024-03-17 17:09:10 +0300

    refactor: Переименовать searchInTitleAndDescription

--- src/main/java/ru/practicum/shareit/item/storage/ItemStorage.java
+++ src/main/java/ru/practicum/shareit/item/storage/ItemStorage.java
@@ -10,5 +10,5 @@ public interface ItemStorage extends JpaRepository<Item, Long> {
     List<Item> findAllByOwnerIdOrderById(Long userId);
 
     @Query("SELECT i FROM Item i WHERE LOWER(i.name) LIKE ?1 OR LOWER(i.description) LIKE ?1 AND i.available = true")
-    List<Item> findAllByNameOrDescriptionContainingIgnoreCase(String text);
+    List<Item> searchInTitleAndDescription(String text);
 }
