efb991f983f75460e0d64dbf3552468e5789d508
vvbakhanovich <vv.bakh@yandex.ru>
2024-03-17 19:48:03 +0300

    javadoc

--- src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
@@ -25,46 +25,78 @@ public class ItemRequestServiceImpl implements ItemRequestService {
     private final UserStorage userStorage;
     private final ItemRequestMapper itemRequestMapper;
 
+    /**
+     * Добавление нового запроса вещи.
+     *
+     * @param userId            идентификатор пользователя, делающего запрос
+     * @param addItemRequestDto описание запрашиваемой вещи
+     * @return заполненный запрос
+     */
     @Override
-    public ItemRequestDto addNewItemRequest(Long userId, AddItemRequestDto addItemRequestDto) {
-        User requester = findUser(userId);
-        ItemRequest itemRequest = itemRequestMapper.toModel(addItemRequestDto);
+    public ItemRequestDto addNewItemRequest(final Long userId, final AddItemRequestDto addItemRequestDto) {
+        final User requester = findUser(userId);
+        final ItemRequest itemRequest = itemRequestMapper.toModel(addItemRequestDto);
         itemRequest.setRequester(requester);
-        ItemRequest savedRequest = itemRequestStorage.save(itemRequest);
+        final ItemRequest savedRequest = itemRequestStorage.save(itemRequest);
         log.info("Добавлен новый запрос с id '{}'.", savedRequest.getId());
         return itemRequestMapper.toDto(savedRequest);
     }
 
+    /**
+     * Получение списка своих запросов вместе с данными ответа на них. Запросы возвращаются в отсортированном порядке
+     * от более новых к более старым.
+     *
+     * @param userId идентификатор пользователя, делающего запрос
+     * @return список запросов
+     */
     @Override
-    public List<ItemRequestDto> getAllItemRequestsFromUser(Long userId) {
+    public List<ItemRequestDto> getAllItemRequestsFromUser(final Long userId) {
         findUser(userId);
-        List<ItemRequest> requests = itemRequestStorage.findRequestsFromUser(userId);
+        final List<ItemRequest> requests = itemRequestStorage.findRequestsFromUser(userId);
         log.info("Получение всех запросов для пользователя с id '" + userId + "'.");
         return itemRequestMapper.toDtoList(requests);
     }
 
+    /**
+     * Получение списка запросов, созданных другими пользователями. Запросы сортируются по дате создания: от более новых
+     * к более старым. Результат возвращается постранично. Для этого указываются два параметра:  from — индекс первого
+     * элемента, начиная с 0, и size — количество элементов для отображения.
+     *
+     * @param userId идентификатор пользователя, делающего запрос
+     * @param from   индекс первого отображаемого элемента, начиная с 0
+     * @param size   количество элементов для отображения
+     * @return список запросов
+     */
     @Override
-    public List<ItemRequestDto> getAvailableItemRequests(Long userId, Long from, Integer size) {
+    public List<ItemRequestDto> getAvailableItemRequests(final Long userId, final Long from, final Integer size) {
         findUser(userId);
         if (from == null && size == null) {
             return itemRequestMapper.toDtoList(itemRequestStorage.findAllRequests());
         }
-        OffsetPageRequest pageRequest = OffsetPageRequest.of(from, size);
-        Page<ItemRequest> requests = itemRequestStorage.findAvailableRequests(userId, pageRequest);
+        final OffsetPageRequest pageRequest = OffsetPageRequest.of(from, size);
+        final Page<ItemRequest> requests = itemRequestStorage.findAvailableRequests(userId, pageRequest);
         log.info("Получение списка запросов, начиная с '{}', по '{}' элемента на странице.", from, size);
         return itemRequestMapper.toDtoList(requests.getContent());
     }
 
+    /**
+     * Получение данных о конкретном запросе вместе с с данными об ответах на него Посмотреть данные об отдельном
+     * запросе может любой пользователь.
+     *
+     * @param userId идентификатор пользователя, делающего запрос
+     * @param requestId идентификатор запроса
+     * @return найденный запрос
+     */
     @Override
-    public ItemRequestDto getItemRequestById(Long userId, Long requestId) {
+    public ItemRequestDto getItemRequestById(final Long userId, final Long requestId) {
         findUser(userId);
-        ItemRequest itemRequest = itemRequestStorage.findById(requestId)
+        final ItemRequest itemRequest = itemRequestStorage.findById(requestId)
                 .orElseThrow(() -> new NotFoundException("Запрос с id '" + requestId + "' не найден."));
         log.info("Получение запроса с id '{}'.", requestId);
         return itemRequestMapper.toDto(itemRequest);
     }
 
-    private User findUser(Long userId) {
+    private User findUser(final Long userId) {
         return userStorage.findById(userId)
                 .orElseThrow(() -> new NotFoundException("Пользователь с id '" + userId + "' не найден."));
     }
