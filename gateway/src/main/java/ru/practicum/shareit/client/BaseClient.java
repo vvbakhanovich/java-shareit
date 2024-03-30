db4b98438d12821d974e9c3e6a3948c348861b80
vvbakhanovich <vv.bakh@yandex.ru>
2024-03-30 13:34:14 +0300

    checkstyle

--- gateway/src/main/java/ru/practicum/shareit/client/BaseClient.java
+++ gateway/src/main/java/ru/practicum/shareit/client/BaseClient.java
@@ -1,16 +1,11 @@
 package ru.practicum.shareit.client;
 
+import org.springframework.http.*;
+import org.springframework.lang.Nullable;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.List;
 import java.util.Map;
-
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestTemplate;
 
 public class BaseClient {
     protected final RestTemplate rest;
